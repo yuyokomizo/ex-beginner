c9f4ec4acd8f9645098b8ed50fd50383ab9c7cc4
yuyokomizo <yu.yokomizo@rakus-partners.co.jp>
2022-02-04 17:06:39 +0900

    form追加

---
+++ src/main/java/com/example/form/UserForm.java
@@ -0,0 +1,27 @@
+package com.example.form;
+
+public class UserForm {
+
+	private String name;
+	private String age;
+	private String comment;
+	
+	public final String getName() {
+		return name;
+	}
+	public final void setName(String name) {
+		this.name = name;
+	}
+	public final String getAge() {
+		return age;
+	}
+	public final void setAge(String age) {
+		this.age = age;
+	}
+	public final String getComment() {
+		return comment;
+	}
+	public final void setComment(String comment) {
+		this.comment = comment;
+	}
+}
